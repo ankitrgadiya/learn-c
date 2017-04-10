2c6f03e5ccdba4809b97ad4716e77e9ec0891b73
Ankit R Gadiya <ankit4922@gmail.com>
2017-04-10 11:18:33 +0530

    Chapter 3 complete!

---
+++ Chapter3/example/prog3.5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	int value1, value2, sum;
+
+	value1 = 50;
+	value2 = 25;
+	
+	sum = value1 + value2;
+
+	printf("Sum of %i and %i is %i.\n", value1, value2, sum);
+
+	return 0;
+}
+
---
+++ Chapter3/exercise/e4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	
+	int result;
+
+	result = 15 - 87;
+	printf("The difference of 15 and 87 is %i\n", result);
+
+	return 0;
+}
